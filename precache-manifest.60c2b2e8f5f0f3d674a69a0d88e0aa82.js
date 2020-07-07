ea5041366c910ada28576e6c6dcb69cc43656110
Rosana Rezende <contato@rosanarezende.com>
2020-07-07 10:31:19 -0300

    Updates

---
+++ precache-manifest.60c2b2e8f5f0f3d674a69a0d88e0aa82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682f02ceaab96899b9c4fb6cfdf7a788",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "804e79fc6dd8ea508998",
+    "url": "/portfolio/static/css/main.463034cd.chunk.css"
+  },
+  {
+    "revision": "d2d4dba13490a73a7342",
+    "url": "/portfolio/static/js/2.ff2977b4.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/portfolio/static/js/2.ff2977b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804e79fc6dd8ea508998",
+    "url": "/portfolio/static/js/main.5194d83b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
